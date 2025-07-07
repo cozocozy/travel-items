56e61f89920bbe1ad655d094ce78b0aa31f79efe
septian <cozocozy@gmail.com>
2025-07-07 14:05:02 +0700

    feat:add stats value

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
         onDeleteItems={handleDeleteItems}
         onToogleItems={handleToogleItems}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -51,7 +51,6 @@ function Form({ onAddItems }) {
     e.preventDefault();
     if (!description) return;
     const newItem = { description, quantity, packed: false, id: Date.now() };
-    console.log(newItem);
     onAddItems(newItem);
     setDescription("");
     setQuantity(1);
@@ -113,10 +112,24 @@ function Item({ item, onDeleteItems, onToogleItems }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list</em>
+      </p>
+    );
+  const numItems = items.length;
+  const packedItems = items.filter((item) => item.packed).length;
+  const percentage = Math.round((packedItems / numItems) * 100);
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? "You got everything you need! Ready to go !!"
+          : `You have ${numItems} items on your list, and you already packed
+        ${packedItems} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
